1850b837356aafeebaa14efd205aa717440bfe90
SiddharthSabat <siddharthsabat@gmail.com>
2021-09-07 16:03:32 +0400

    Train and deploy model using AWS Sagemaker

--- Project_Plagiarism_Detection/source_sklearn/train.py
+++ Project_Plagiarism_Detection/source_sklearn/train.py
@@ -7,9 +7,11 @@ import pandas as pd
 # sklearn.externals.joblib is deprecated in 0.21 and will be removed in 0.23. 
 # from sklearn.externals import joblib
 # Import joblib package directly
-import joblib
+#import joblib
+from sklearn.externals import joblib
 
 ## TODO: Import any additional libraries you need to define a model
+from sklearn.svm import LinearSVC
 
 
 # Provided model load function
@@ -58,12 +60,12 @@ if __name__ == '__main__':
     ## --- Your code here --- ##
     
 
-    ## TODO: Define a model 
-    model = None
+    ## TODO: Define a model
+    model = LinearSVC()
     
     
     ## TODO: Train the model
-    
+    model.fit(train_x, train_y)
     
     
     ## --- End of your code  --- ##
